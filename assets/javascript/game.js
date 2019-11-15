3b0235f43ac3af494f8c849dbaf6bee7492e9f90
Edward Hill <edwardpassanohill@gmail.com>
2019-11-15 17:49:20 -0500

    Add psudocode for JS

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,1 +1,16 @@
-// alert("testing");
+// generate a random number to be matched
+// display that random number to <div class="scoreboard..."></div>
+// display wins to <div class="winsandlosses ...">
+// display losses <div class="winsandlosses ...">
+// assign random value to 4 flies
+//--
+// if click fly 1 add value to total score,
+// if click fly 2 add value to total score,
+// if click fly 3 add value to total score,
+// if click fly 4 add value to total score,
+// if score is greater than random number, you lose, write loss to Losses <p>
+// if score = random number, you win, write win to Win <p>
+//---
+// if win OR lose
+// 1) randomize number again
+// 2) randomize flies value
