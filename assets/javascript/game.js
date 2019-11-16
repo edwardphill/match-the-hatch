590145c626f501bdb96f9fdde0c084d381cbf6c5
Edward Hill <edwardpassanohill@gmail.com>
2019-11-16 17:04:40 -0500

    Add first steps of logic, click(), add score, generate random numbers for elements

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,16 +1,81 @@
 // generate a random number to be matched
-// display that random number to <div class="scoreboard..."></div>
-// display wins to <div class="winsandlosses ...">
-// display losses <div class="winsandlosses ...">
+
+var randomNumber = Math.floor(Math.random() * 100 + 1);
+var caddis = Math.floor(Math.random() * 10 + 1);
+var olive = Math.floor(Math.random() * 10 + 1);
+var wooly = Math.floor(Math.random() * 10 + 1);
+var tail = Math.floor(Math.random() * 10 + 1);
+var wins;
+var losses;
+var totalScore;
+
+// display that random number to <div class="scoreboard..."></div> (push)
+
+$("#score").html(randomNumber);
+
+// display wins to <id="wins">
+
+$("#wins").html(wins);
+
+// start showing 0
+
+// display losses <id=#losses">
+
+$("#losses").html(losses);
+
+// start showing 0
+
 // assign random value to 4 flies
-//--
-// if click fly 1 add value to total score,
-// if click fly 2 add value to total score,
-// if click fly 3 add value to total score,
-// if click fly 4 add value to total score,
+// create an array of elkhair bwo, wb, pt and loop through that array to generate a random number for each html elemnts
+
+// $("#elkHairCaddis").html(caddis);
+console.log(caddis);
+// $("#blueWingedOlive").html(olive);
+console.log(olive);
+// $("#woolyBugger").html(wooly);
+console.log(wooly);
+// $("#pheasantTail").html(tail);
+console.log(tail);
+
+// if click fly 1 add value to total score, add event listener click()
+
+// if I click elkhaircaddis, add the caddis value to id=totalScore
+
+// provide with funtion to increment score to .click()
+
+/*function increaseScore() {
+    if (click)
+}*/
+
+$("#elkHairCaddis").on("click", function() {
+  $("#totalScore").html(caddis);
+  console.log("click");
+});
+
+// if you click fly 2 add value to total score, add event listener click()
+
+$("#blueWingedOlive").on("click", function() {
+  $("#totalScore").html(olive);
+  console.log("click");
+});
+
+// if click fly 3 add value to total score, add event listener click()
+$("#woolyBugger").on("click", function() {
+  $("#totalScore").html(wooly);
+  console.log("click");
+});
+// if click fly 4 add value to total score, add event listener click()
+$("#pheasantTail").on("click", function() {
+  $("#totalScore").html(tail);
+  console.log("click");
+});
+
 // if score is greater than random number, you lose, write loss to Losses <p>
+
+/* if ("#score" > i) {
+  alert("you lose!");
+} */
+
 // if score = random number, you win, write win to Win <p>
-//---
-// if win OR lose
-// 1) randomize number again
-// 2) randomize flies value
+
+// if win OR lose, randomize number again and randomize flies
